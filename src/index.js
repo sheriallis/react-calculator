d7a6db32a7d1f2dd55d15ba7ccbd955f944902de
sheriallis <shrdson@gmail.com>
2019-07-12 22:25:56 +0200

    Add styling

--- src/index.js
+++ src/index.js
@@ -11,22 +11,22 @@ class App extends React.Component{
         <ReactFCCtest />
         <div id="display">0</div>
         <button id="clear">AC</button>
-        <button id="divide">&divide;</button>
-        <button id="multiply">&times;</button>
+        <button id="divide" className="operator">&divide;</button>
+        <button id="multiply" className="operator">&times;</button>
         <button id="seven">7</button>
         <button id="eight">8</button>
         <button id="nine">9</button>
-        <button id="subtract">&minus;</button>
+        <button id="subtract" className="operator">&minus;</button>
         <button id="four">4</button>
         <button id="five">5</button>
         <button id="six">6</button>
-        <button id="add">&plus;</button>
+        <button id="add" className="operator">+</button>
         <button id="one">1</button>
         <button id="two">2</button>
         <button id="three">3</button>
-        <button id="equals">=</button>
         <button id="zero">0</button>
         <button id="decimal">.</button>
+        <button id="equals" className="operator">=</button>
       </div> 
     )
   }
